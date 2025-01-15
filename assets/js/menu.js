acbe1e3d0a2b04c5c83429fbddfb8094c34e2375
Andreas Weizel <rpinski@awzhome.de>
2025-01-16 00:11:57 +0100

    Extend menu styling JavaScript

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,9 +1,18 @@
 const el = document.getElementById("menu-toggle");
-if (el) {
+const header = document.getElementById("menu-header");
+
+if (el && header) {
   el.addEventListener("click", (event) => {
     event.preventDefault();
     const target = document.getElementById("menu");
     el.ariaExpanded = target.classList.contains("hidden");
     target.classList.toggle("hidden");
+    header.classList.toggle('expanded')
   });
+}
+
+window.onload = function() {
+  window.onscroll = function() {
+      header.classList.toggle("scrolled", window.scrollY > 32);
+  }
 }
